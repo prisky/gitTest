6725fb2e8f08ffc35cac452c5b5c46ff3f42a471
Hayden Prisk <hayden@haydens-mbp.home>
2018-08-28 12:19:56 +1000

    compete create project

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Hayden Prisk on 28/8/18.
+//  Copyright © 2018 Hayden Prisk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
